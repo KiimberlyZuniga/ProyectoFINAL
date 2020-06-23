83edc75b662dee3eaffe5bf4de4665e91ea2d9d5
Kim Zúñiga <60992987+KiimberlyZuniga@users.noreply.github.com>
2020-06-23 16:21:14 -0600

    Primeras Tres pantallas Proyecto

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sigienteVentana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sigienteVentana = (Button)findViewById(R.id.nextActivity);
+
+        sigienteVentana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguiente = new Intent(MainActivity.this, PantallaUno_IncendioReportando.class);
+                startActivity(siguiente);
+
+            }
+        });
     }
 }

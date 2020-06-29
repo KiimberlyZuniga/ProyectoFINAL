32ec7eb76d6bb24f370c6737e0a2db8e3e45fe80
Kim Zúñiga <60992987+KiimberlyZuniga@users.noreply.github.com>
2020-06-28 23:35:24 -0600

    arreglos y login

--- app/src/main/java/com/example/proyectofinal/PantallaTres_IncendioPendiente.java
+++ app/src/main/java/com/example/proyectofinal/PantallaTres_IncendioPendiente.java
@@ -1,8 +1,8 @@
 package com.example.proyectofinal;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class PantallaTres_IncendioPendiente extends AppCompatActivity {
 
--- app/src/main/java/com/example/proyectofinal/PantallaUno_IncendioReportando.java
+++ app/src/main/java/com/example/proyectofinal/PantallaUno_IncendioReportando.java
@@ -14,24 +14,83 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
+import static com.example.proyectofinal.R.layout.activity_pantalla_uno__incendio_reportando;
+
 public class PantallaUno_IncendioReportando extends AppCompatActivity {
 
     FloatingActionButton botonFlotante;
     TextView name, email;
     Button logOut;
+    Button incendPendButt;
+    //TABS
+   // TabLayout tabLayout;
+    //ViewPager viewPager;
+   // TabItem tab1;
+   // TabItem tab2;
+   // TabItem tab3;
+   // PagerController pagerAdater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_uno__incendio_reportando);
+        setContentView(activity_pantalla_uno__incendio_reportando);
+
+
+        //TABS
+
+       // tabLayout = findViewById(R.id.tablayout);
+        //viewPager = findViewById(R.id.viewpager);
+        //tab1 = findViewById(R.id.tabincendiopend);
+        //tab2 = findViewById(R.id.tabinformationn);
+        //tab3 = findViewById(R.id.tabcontactus);
+     /*   pagerAdater = new PagerController(getSupportFragmentManager(), tabLayout.getTabCount());
+        //viewPager.setAdapter(pagerAdater);
+        tabLayout.setOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+               // viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0){
+                    pagerAdater.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==1){
+                    pagerAdater.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==2){
+                    pagerAdater.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+*/
+        //Bton FLOTANTE
 
         botonFlotante = (FloatingActionButton) findViewById(R.id.BotonFlotante1);
+        incendPendButt = (Button) findViewById(R.id.butIncendioPend);
 
         botonFlotante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent siguienteActivity = new Intent(PantallaUno_IncendioReportando.this, PantallaDos_ReportarIncendio.class);
                 startActivity(siguienteActivity);
+            }
+        });
+
+        incendPendButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent incendPendActiv = new Intent(PantallaUno_IncendioReportando.this, PantallaTres_IncendioPendiente.class);
+                startActivity(incendPendActiv);
             }
         });
 
@@ -54,5 +113,7 @@ public class PantallaUno_IncendioReportando extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
